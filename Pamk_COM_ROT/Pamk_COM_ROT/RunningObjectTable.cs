fec3c742a18320148a2fee527a812a13cd348440
Peter Kriegel <schickmirpost@web.de>
2014-11-18 18:55:47 +0100

    added correct Visual Studio Project
    added correct Visual Studio Project

---
+++ Pamk_COM_ROT/Pamk_COM_ROT/RunningObjectTable.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+/*
+ * Credits to the orginators:
+ * Orginal Code from Rainbird Year: 2007
+ * See: http://dotnet-snippets.de/snippet/laufende-com-objekte-abfragen/526
+ * Modified by: Sebastian.Lange Year 2012
+ * See: http://www.mycsharp.de/wbb2/thread.php?threadid=36340
+ */
+
+namespace Pamk_COM_ROT
+{
+    /// <summary>
+    /// This class provides direct access to the Running Object Table (ROT) which holds the current runnig COM Objects
+    /// The Running Object Table (ROT) is a machine-wide table in which objects can register themselves.
+    /// Because it is optional for a COM object to register within the ROT you can not find all running COM Objects inside this table
+    /// Even sometimes you will find only one entry in the ROT for an COM object which runs many instances. This is by design of the COM producer.  
+    /// </summary>
+    public static class RunningObjectTable
+    {
+        /// <summary>
+        /// Private default constructor
+        /// </summary>
+        //private RunningObjectTable() { }
+
+        // Win32 API call to read the ROT
+        [DllImport("ole32.dll")]
+        private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
+
+        // Win32 API call to create the binding
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(uint reserved, out IBindCtx pctx);
+
+        public static List<RunningObjectTableComponentInfo> GetComponentsFromROT()
+        {
+            IEnumMoniker monikerList = null;
+            IRunningObjectTable runningObjectTable = null;
+            List<RunningObjectTableComponentInfo> resultList = new List<RunningObjectTableComponentInfo>();
+            try
+            {
+                // query table and returns null if no objects runnings
+                if (GetRunningObjectTable(0, out runningObjectTable) != 0 || runningObjectTable == null)
+                    return null;
+
+                // query moniker & reset
+                runningObjectTable.EnumRunning(out monikerList);
+                monikerList.Reset();
+
+                IMoniker[] monikerContainer = new IMoniker[1];
+                IntPtr pointerFetchedMonikers = IntPtr.Zero;
+
+                // fetch all moniker
+                while (monikerList.Next(1, monikerContainer, pointerFetchedMonikers) == 0)
+                {
+                    // create binding object
+                    IBindCtx bindInfo;
+                    CreateBindCtx(0, out bindInfo);
+
+                    // query com proxy info      
+                    object comInstance = null;
+                    runningObjectTable.GetObject(monikerContainer[0], out comInstance);
+
+                    string ppszDisplayName;
+                    try { monikerContainer[0].GetDisplayName(bindInfo, null, out ppszDisplayName); }
+                    catch { ppszDisplayName = ""; }
+
+                    Guid pClassID;
+                    try { monikerContainer[0].GetClassID(out pClassID); }
+                    catch { pClassID = Guid.Empty; }
+                    
+                    System.Runtime.InteropServices.ComTypes.FILETIME pLastChangedFileTime;
+                    try { runningObjectTable.GetTimeOfLastChange(monikerContainer[0], out pLastChangedFileTime);}
+                    catch { pLastChangedFileTime = new System.Runtime.InteropServices.ComTypes.FILETIME(); }
+                    
+                    long pcbSize;
+                    try { monikerContainer[0].GetSizeMax(out pcbSize);}
+                    catch { pcbSize = 0;}
+                    
+                    
+                    Boolean IsDirty;
+                    try {IsDirty = (monikerContainer[0].IsDirty() == 0); }
+                    catch {IsDirty = false;}
+
+                    Boolean IsRunning;
+                    try {IsRunning = (runningObjectTable.IsRunning(monikerContainer[0]) == 0); }
+                    catch {IsRunning = false;}
+
+                    // creating the unbound object to hold the Data out of the current component IMoniker
+                    RunningObjectTableComponentInfo ROTComponent = new RunningObjectTableComponentInfo(
+                        ppszDisplayName,
+                        pClassID,
+                        ConvertFromFILETIME(pLastChangedFileTime),
+                        pcbSize,
+                        TypeDescriptor.GetComponentName(comInstance, false),
+                        TypeDescriptor.GetClassName(comInstance),
+                        IsRunning,
+                        IsDirty
+                    );
+
+                    resultList.Add(ROTComponent);
+
+                    // clean up and release object 
+                    Marshal.ReleaseComObject(comInstance);
+                    Marshal.ReleaseComObject(bindInfo);
+                }
+
+                // not running
+                return resultList;
+            }
+            finally
+            {
+                // release proxies
+                if (runningObjectTable != null)
+                    Marshal.ReleaseComObject(runningObjectTable);
+                if (monikerList != null)
+                    Marshal.ReleaseComObject(monikerList);
+            }
+        }
+
+        public static object GetInstanceFromROT(RunningObjectTableComponentInfo RunningComponent)
+        {
+            IEnumMoniker monikerList = null;
+            IRunningObjectTable runningObjectTable = null;
+            object ResultInstance = null;
+
+            try
+            {
+                // query table and returns null if no objects runnings
+                if (GetRunningObjectTable(0, out runningObjectTable) != 0 || runningObjectTable == null)
+                    return null;
+
+                // query moniker & reset
+                runningObjectTable.EnumRunning(out monikerList);
+                monikerList.Reset();
+
+                IMoniker[] monikerContainer = new IMoniker[1];
+                IntPtr pointerFetchedMonikers = IntPtr.Zero;
+
+                // fetch all moniker
+                while (monikerList.Next(1, monikerContainer, pointerFetchedMonikers) == 0)
+                {
+                    // create binding object
+                    IBindCtx bindInfo;
+                    CreateBindCtx(0, out bindInfo);
+
+                    // query com proxy info      
+                    object comInstance = null;
+                    runningObjectTable.GetObject(monikerContainer[0], out comInstance);
+
+                    string ppszDisplayName;
+                    try { monikerContainer[0].GetDisplayName(bindInfo, null, out ppszDisplayName); }
+                    catch { ppszDisplayName = ""; }
+
+                    Guid pClassID;
+                    try { monikerContainer[0].GetClassID(out pClassID); }
+                    catch { pClassID = Guid.Empty; }
+
+                    string ClassName;
+                    try { ClassName = TypeDescriptor.GetClassName(comInstance); }
+                    catch { ClassName = ""; }
+
+                    string ComponentName;
+                    try { ComponentName = TypeDescriptor.GetComponentName(comInstance, false); }
+                    catch { ComponentName = ""; }
+
+                    if ((RunningComponent.DisplayName == ppszDisplayName) &&
+                        (RunningComponent.ClsID == pClassID) &&
+                        (RunningComponent.ComponentClassName == ClassName) &&
+                        (RunningComponent.ComponentName == ComponentName)
+                        )
+                    {
+                        Marshal.ReleaseComObject(bindInfo);
+                        ResultInstance = comInstance;
+                    }
+                    else
+                        Marshal.ReleaseComObject(comInstance);
+
+                    Marshal.ReleaseComObject(bindInfo);
+                }
+                
+                return ResultInstance;
+            }
+            finally
+            {
+                // release proxies
+                if (runningObjectTable != null)
+                    Marshal.ReleaseComObject(runningObjectTable);
+                if (monikerList != null)
+                    Marshal.ReleaseComObject(monikerList);
+            }
+        }
+
+        //convert from System.Runtime.InteropServices.ComTypes.FILETIME to the local System.DateTime
+        // The COM FILETIME is based on coordinated universal time (UTC). UTC-based time is loosely defined as the current date and time of day in Greenwich, England.
+        // this function uses the current system settings for the time zone (differenc from UTC to your timezone) and daylight saving time (summer- / winter-time settings).
+        static DateTime ConvertFromFILETIME(System.Runtime.InteropServices.ComTypes.FILETIME FileTime)
+        {
+            long highBits = FileTime.dwHighDateTime;
+            highBits = highBits << 32;
+            try { return DateTime.FromFileTimeUtc(highBits | (long)(uint)FileTime.dwLowDateTime).ToLocalTime(); }
+            catch { return DateTime.MinValue; }
+        }
+
+        //convert from System.DateTime to System.Runtime.InteropServices.ComTypes.FILETIME
+        static System.Runtime.InteropServices.ComTypes.FILETIME ConvertToFILETIME(DateTime TimeAndDate)
+        {
+            System.Runtime.InteropServices.ComTypes.FILETIME FileTime = new System.Runtime.InteropServices.ComTypes.FILETIME();
+            long dtFileTime = TimeAndDate.ToFileTime();
+            FileTime.dwLowDateTime = (int)(dtFileTime & 0xFFFFFFFF);
+            FileTime.dwHighDateTime = (int)(dtFileTime >> 32);
+            return FileTime;
+        }
+    } // end class RunningObjectTable
+} // end namespace
---
+++ Pamk_COM_ROT/Pamk_COM_ROT/RunningObjectTableComponentInfo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pamk_COM_ROT
+{
+    // Simple class to create an unbound object to hold data out of an
+    // Component of the Running Object Table (ROT) 
+    public class RunningObjectTableComponentInfo
+    {
+        // constructor
+        public RunningObjectTableComponentInfo(
+            String DisplayName,
+            Guid ClassID, DateTime LastChanged,
+            long SizeMax,
+            String ComponentName,
+            String ComponentClassName,
+            Boolean IsRunning,
+            Boolean IsDirty
+         )
+        {
+            _DisplayName = DisplayName;
+            _ClsID = ClassID;
+            _LastChanged = LastChanged;
+            _SizeMax = SizeMax;
+            _ComponentName = ComponentName;
+            _ComponentClassName = ComponentClassName;
+            _IsRunning = IsRunning;
+            _IsDirty = IsDirty;
+        }
+
+#region properties
+        private String _DisplayName;
+        public String DisplayName
+        {get { return _DisplayName; }}
+
+        private Guid _ClsID;
+        public Guid ClsID
+        {get { return _ClsID; }}
+
+        internal DateTime _LastChanged;
+        public DateTime LastChanged
+        {
+            get {
+                this.Refresh();
+                return _LastChanged;
+            }
+        }
+
+        private long _SizeMax;
+        public long SizeMax
+        {get { return _SizeMax; }}
+
+        private String _ComponentName;
+        public String ComponentName
+        {get { return _ComponentName; }}
+
+        private String _ComponentClassName;
+        public String ComponentClassName
+        {get { return _ComponentClassName; }}
+
+        internal Boolean _IsRunning = false;
+        public Boolean IsRunning
+        {
+            get {
+                this.Refresh();
+                return _IsRunning;
+            }
+        }
+
+        internal Boolean _IsDirty = false;
+        public Boolean IsDirty
+        {
+            get {
+                this.Refresh();
+                return _IsDirty;
+            }
+        }
+
+#endregion // properties
+
+        // function to refresh the property values of an RunningObjectTableComponent object 
+        private void Refresh()
+        {
+            // set default values 
+            this._IsDirty = false;
+            this._IsRunning = false;
+
+            // refresh the properties of the component by a call to GetComponentsFromROT()
+            foreach (RunningObjectTableComponentInfo RunningROTComponent in RunningObjectTable.GetComponentsFromROT())
+            {
+                // if the object is still in the ROT table we refresh the property values
+                if (RunningROTComponent.DisplayName == this.DisplayName)
+                {
+                    // if the object is still in the ROT table and the refreshed property _IsRunning is still true we return true
+                    this._IsDirty = RunningROTComponent._IsDirty;
+                    // if the object is still in the ROT table and the refreshed property _IsRunning is still true we return true
+                    this._IsRunning = RunningROTComponent._IsRunning;
+                    // if the object is still in the ROT table we return the refreshed LastChanged DateTime
+                    this._LastChanged = RunningROTComponent._LastChanged;
+                    break;
+                }
+            }
+        }
+        
+        // if this object is still in the Running Object Table (ROT) we try to return the reference to it
+        public object GetInstance() {
+            return RunningObjectTable.GetInstanceFromROT(this);
+        }
+
+    }  // end class RunningObjectTableComponent
+}
